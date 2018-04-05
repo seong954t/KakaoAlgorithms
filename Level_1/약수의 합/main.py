3cfcd13fd3013db0eeca2c4051f95c7676263029
seong954t <seomtae@naver.com>
2018-04-05 10:11:40 +0900

    약수의 합 update

--- Level_1/약수의 합/main.py
+++ Level_1/약수의 합/main.py
@@ -1,6 +1,6 @@
 def sumDivisor(num):
-    answer = 0
-    for i in range(1, num+1):
+    answer = num
+    for i in range(1, int(num/2)+1):
         if num % i == 0:
             answer += i
     return answer
