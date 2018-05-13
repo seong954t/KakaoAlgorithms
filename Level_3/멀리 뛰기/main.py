89d3e2daf5e905c21f164d7bcf1c8b1e28bdeb6d
seong954t <seomtae@naver.com>
2018-05-13 16:39:13 +0900

    멀리 뛰기 수정

--- Level_3/멀리 뛰기/main.py
+++ Level_3/멀리 뛰기/main.py
@@ -1,9 +1,9 @@
 def jumpCase(num):
     answer = 0
-    dp = [0, 1, 2]
+    a, b = 1, 2
     for i in range(3, num+1):
-        dp.append(dp[i-1]+dp[i-2])
-    return dp[len(dp)-1]
+        a, b = b, a+b
+    return b
 
 #아래는 테스트로 출력해 보기 위한 코드입니다.
 print(jumpCase(4))
