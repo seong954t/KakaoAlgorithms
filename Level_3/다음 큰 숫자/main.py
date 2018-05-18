fd3550f598482aa6cf2f8b1c5ca21206e46afc7b
seong954t <seomtae@naver.com>
2018-05-18 12:21:51 +0900

    update files

--- Level_3/다음 큰 숫자/main.py
+++ Level_3/다음 큰 숫자/main.py
@@ -1,9 +1,9 @@
 def nextBigNumber(n):
-    count = bin(n).count('1')
-    n += 1
-    while bin(n).count('1') != count:
-        n += 1
-    return n
+    binN = list(bin(n))[2:]
+    binN.reverse()
+    lastOneIndex = binN.index('1')
+    subNum = bin(n).count('1') - bin(n+2**lastOneIndex).count('1')
+    return n + (2 ** lastOneIndex) + (2 ** subNum) -1
 
 #아래 코드는 테스트를 위한 출력 코드입니다.
 print(nextBigNumber(78))
