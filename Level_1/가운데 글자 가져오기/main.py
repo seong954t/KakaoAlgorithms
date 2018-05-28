631533173d332d53143e03faf47d68b692dd8463
seong954t <seomtae@naver.com>
2018-05-28 10:49:29 +0900

    가운데 글자 가져오기 수정

--- Level_1/가운데 글자 가져오기/main.py
+++ Level_1/가운데 글자 가져오기/main.py
@@ -1,11 +1,11 @@
-def string_middle(str):
+def solution(str):
     # 함수를 완성하세요
     str_len = len(str)
-    str_len_2 = int(str_len/2)
+    str_len_2 = str_len//2
     if str_len%2 == 0:
         return str[str_len_2-1:str_len_2+1]
     else:
         return str[str_len_2:str_len_2+1]
 
 # 아래는 테스트로 출력해 보기 위한 코드입니다.
-print(string_middle("powwer"))
+print(solution("powwer"))
