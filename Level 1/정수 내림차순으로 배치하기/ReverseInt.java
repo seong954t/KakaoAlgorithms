d363a1e6ba0a523b7293f6f2e0d5d34240e79b08
seong954t <seomtae@naver.com>
2018-06-10 23:53:34 +0900

    정수 내림차순으로 배치하기 update

--- Level 1/정수 내림차순으로 배치하기/ReverseInt.java
+++ Level 1/정수 내림차순으로 배치하기/ReverseInt.java
@@ -1,25 +1,17 @@
-public class ReverseInt {
-	public int reverseInt(int n){
-    int result = 0;
-    int []arr = new int[10];
-    while(n > 0){
-      arr[n%10]++;
-      n /= 10;
-    }
-    for(int i = 9; i>=0; i--){
-    	for(int j = 0; j<arr[i]; j++){
-        if(arr[i] > 0){
-        	result *= 10;
-      		result += i;
-        }
+class Solution {
+  public long solution(long n) {
+      long result = 0;
+      int []arr = new int[10];
+      while(n > 0){
+          arr[(int)(n%10)]++;
+          n /= 10;
       }
-    }
-		return result;
-	}
-  
-	// 아래는 테스트로 출력해 보기 위한 코드입니다.
-	public static void  main(String[] args){
-		ReverseInt ri = new ReverseInt();
-		System.out.println(ri.reverseInt(118372));
-	}
+      for(int i = 9; i>=0; i--){
+          for(int j = 0; j<arr[i]; j++){
+              result *= 10;
+              result += i;
+          }
+      }
+  return result;
+  }
 }
