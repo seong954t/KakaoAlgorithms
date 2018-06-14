265efb2c8d36d736bfe73eeed74facc1e70396bc
seong954t <seomtae@naver.com>
2018-06-14 19:44:35 +0900

    선입선출 스케줄링 TODO

--- Level 4/선입선출 스케줄링/main.py
+++ Level 4/선입선출 스케줄링/main.py
@@ -1,39 +1,18 @@
 def solution(n, cores):
-    answer = 0
-    idx = 0
-    prev_idx = 0
-    next_idx = 0
-    br = 0
-    while True:
-        _sum = 0
-        next_sum = 0
-        for c in cores:
-            _sum += idx//c+1
-            next_sum += (idx+1)//c+1
-            
-        if _sum < n and next_sum >= n:
-            break
+    row = 1
+    total_sum = n-len(cores)
+    # TODO :: ROW를 구하는데 시간이 오래걸림
+    while total_sum > cal_row_val(row, cores):
+        row += 1
+    total_task = cal_row_val(row-1, cores)+len(cores)
+    for idx in range(len(cores)):
+        if row % cores[idx] == 0:
+            total_task += 1
+        if total_task == n:
+            return idx+1
 
-        if _sum < n:
-            prev_idx = idx
-            if next_idx > 0:
-                idx = (idx+next_idx)//2
-            else:
-                idx = (idx+1)*2
-        else:
-            next_idx = idx
-            idx = (prev_idx+idx)//2
-    
-    _sum = 0
-    for c in cores:
-        _sum += idx//c+1
-    
-    idx += 1
-    for c in cores:
-        answer += 1
-        if idx%c == 0:
-            _sum += 1
-        if _sum == n:
-            break
-            
-    return answer
+def cal_row_val(row, cores):
+    result = 0
+    for core in cores:
+        result +=  row // core
+    return result
